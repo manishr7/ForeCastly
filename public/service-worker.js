3a1802c4d8a72df734303a8975425e02d91102b6
manishr7 <143963938+manishr7@users.noreply.github.com>
2024-09-23 04:27:39 +0530

    Error free commit

---
+++ public/service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "weather-app-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/static/js/bundle.js",
+  "/static/js/main.chunk.js",
+  "/static/js/0.chunk.js",
+  "/favicon.ico",
+  
+];
+
+
+this.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+
+this.addEventListener("fetch", (event) => {
+  if(!navigator.onLine){
+  
+   
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        if(response)
+        return response ; 
+      })
+    );
+}
+});
