008d7387ba9e4ddda9ada9139fbcb44053f04e56
manishr7 <143963938+manishr7@users.noreply.github.com>
2024-09-23 01:55:37 +0530

    final commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,26 +1,30 @@
 const CACHE_NAME = "weather-app-cache-v1";
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "/static/js/bundle.js", 
-  "./favicon.ico",
-  "./manifest.json",
+  "/",                      // Main page
+  "/index.html",             // Main HTML file
+  "/static/js/bundle.js",    // JS bundle
+  "/favicon.ico",            // Favicon
+  "/manifest.json"           // Manifest file
 ];
 
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+    event.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        try {
+          return cache.addAll(urlsToCache);
+        } catch (error) {
+          console.error("Failed to cache resources:", error);
+        }
+      })
+    );
+  });
 
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-    
+      
       return response || fetch(event.request);
     })
   );
